249375bcc13d469eb7b018e7033f875ef17a472f
Samuel Liu <spliu.98@gmail.com>
2020-05-13 03:37:15 -0700

    Add files via upload

---
+++ Average_Donation.js
@@ -0,0 +1,97 @@
+import { Select, Modal, Button } from 'antd';
+import React, {Component} from 'react';
+import axios from "axios";
+
+const { Option } = Select;
+
+export default class AverageDonation extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {visible: false, value: "Project Category", donationss: 0, backers: 0, backerdonate: 0};
+        
+    this.handleChange = this.handleChange.bind(this);
+    this.handleOk = this.handleOk.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.showModal = this.showModal.bind(this);
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    //route to calculation function?
+   console.log("value: ", this.state.value);
+   var values = {"value": this.state.value,
+                 "donationss": 0,
+                 "backers": 0,
+                 "backerdonate": 0
+                }
+   console.log(values);
+   this.getDonation(values);
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleChange = (id, e) =>{
+      this.setState({value: e}); 
+  };
+
+  dropDownValue(value) {
+      console.log(value);
+  }
+
+  getDonation = async values => {
+      const result = await axios.post("http://localhost:9000/datasetpoint/getDonation", values);
+      console.log(typeof result.data);
+      this.setState({donationss: result.data.donationss});
+      this.setState({backers: result.data.backers});
+      this.setState({backerdonate: result.data.backerdonate});
+  }
+
+  render() {
+    return (
+      <div>
+        <Button size="large" type="primary" onClick={this.showModal}>
+          Average Donation Raised
+        </Button>
+        <Modal
+          title="Average Amount of Money Raised by Category"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          okText="Calculate"
+        >
+          <div>
+            <Select value={this.state.value} onChange={(e) => this.handleChange(0, e)}>
+              {/*<Option value="Music">Music</Option>*/}
+              <Option value="Food">Food</Option>
+              <Option value="Design">Design</Option>
+              <Option value="Games">Games</Option>
+              <Option value="Publishing">Publishing</Option>
+              {/*<Option value="Film & Video">Film and Video</Option>*/} 
+              <Option value="Fashion">Fashion</Option>
+              <Option value="Technology">Technology</Option>
+              <Option value="Crafts">Crafts</Option>
+              <Option value="Art">Art</Option>
+            </Select>
+          </div>
+          <div>
+              <p>The average donation of {this.state.value} is ${this.state.donationss}</p>
+              <p>The total number of backers for {this.state.value} is {this.state.backers}</p>
+              <h5>How much would each backer be donating to {this.state.value} on average given these quantities?</h5>
+              <p>For {this.state.value}, since there is an average of {this.state.backers} backers with an average overall category donation of ${this.state.donationss}, each backer would be donating roughly about ${this.state.backerdonate}.</p>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
---
+++ Top_Categories.js
@@ -0,0 +1,106 @@
+import { Select, Modal, Button } from 'antd';
+import React, {Component} from 'react';
+import axios from "axios";
+
+const { Option } = Select;
+
+export default class TopCategories extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {visible: false, value: 0, top1: "", count1: 0, top2: "", count2: 0, top3: "", count3: 0, top4: "", count4: 0, top5: "", count5: 0};
+        
+    this.handleChange = this.handleChange.bind(this);
+    this.handleOk = this.handleOk.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.showModal = this.showModal.bind(this);
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log("value: " , this.state.value);
+    var values = {"value": this.state.value, 
+                  "top1": "",
+                  "top2": "",
+                  "top3": "",
+                  "top4": "",
+                  "top5": "",
+                  "count1": 0,
+                  "count2": 0,
+                  "count3": 0,
+                  "count4": 0,
+                  "count5": 0
+                }
+    this.getTop(values);
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleChange = (e) =>{
+    this.setState({value: e})
+  };
+
+  getTop = async values => {
+    const result = await axios.post("http://localhost:9000/datasetpoint/getTop", values);
+    console.log(result.data);
+    this.setState({top1: result.data.top1});
+    this.setState({top2: result.data.top2});
+    this.setState({top3: result.data.top3});
+    this.setState({top4: result.data.top4});
+    this.setState({top5: result.data.top5});
+    this.setState({count1: result.data.count1});
+    this.setState({count2: result.data.count2});
+    this.setState({count3: result.data.count3});
+    this.setState({count4: result.data.count4});
+    this.setState({count5: result.data.count5});
+  };
+
+  render() {
+    return (
+      <div>
+        <Button size="large" type="primary" onClick={this.showModal}>
+          Most Common Projects (Ranked)
+        </Button>
+        <Modal
+          title="Top 5 Common Categories"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          okText="Submit"
+        >
+          <div>
+            <h3>This metric shows the most common kind of project type ranked in order (top 5)</h3>
+          </div>
+
+          <div>
+            <p>The category with the Most of its type is: {this.state.top1}</p>
+            <p>The category with the Second most of its type is: {this.state.top2}</p>
+            <p>The category with the Third most of its type is: {this.state.top3}</p>
+            <p>The category with the Fourth most of its type is: {this.state.top4}</p>
+            <p>The category with the Fifth most of its type is: {this.state.top5}</p>
+            
+            <h3>Detailed View (Total Number for Each Project)</h3>
+
+            <p>{this.state.top1} has {this.state.count1} projects.</p>
+            <p>{this.state.top2} has {this.state.count2} projects.</p>
+            <p>{this.state.top3} has {this.state.count3} projects.</p>
+            <p>{this.state.top4} has {this.state.count4} projects.</p>
+            <p>{this.state.top5} has {this.state.count5} projects.</p>
+
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
